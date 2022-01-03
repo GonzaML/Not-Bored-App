a39d145a47b5bd7865f94b379e525e3560b6c2dc
GonzaML <gonzalo.ocampo@mercadolibre.com>
2022-01-03 18:12:54 -0300

    Adding the activity, and retrofit classes

--- app/src/main/java/com/example/notbored/Activities.kt
+++ app/src/main/java/com/example/notbored/Activities.kt
@@ -2,17 +2,24 @@ package com.example.notbored
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.notbored.databinding.ActivityMainBinding
 
 class Activities : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_activities)
+        setContentView(binding.root)
+        //initRecylcerView()
+
+        }
+
+    /*private fun initRecylcerView() {
+
+        adapter = ActivityAdapter(activityTypeList)
+        binding.rvTypes.adapter = adapter
 
 
-
-
-
-
-    }
+    }*/
 
 }
--- app/src/main/java/com/example/notbored/MainActivity.kt
+++ app/src/main/java/com/example/notbored/MainActivity.kt
@@ -4,33 +4,72 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.notbored.databinding.ActivityActivitiesBinding
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import com.example.notbored.databinding.ActivityMainBinding
+import com.example.notbored.retroFit.APIServiceActivity
+import com.example.notbored.retroFit.ActivityResponse
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var  adapter : ActivityAdapter
+    private var activityTypeList = listOf<ActivityResponse>()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val buttonTermsAndConditions = findViewById<Button>(R.id.termsAndConditions)
-        buttonTermsAndConditions.setOnClickListener {
-            val termsAndConditionsIntent = Intent(this, TermsAndConditions::class.java)
-            startActivity(termsAndConditionsIntent)
+        binding.termsAndConditions.setOnClickListener{
+            setContentView(R.layout.activity_terms_and_conditions)
         }
 
-        val buttonStart = findViewById<Button>(R.id.startButton)
-        buttonStart.setOnClickListener {
-            val startIntent = Intent(this, Activities::class.java)
-            startActivity(startIntent)
+        binding.startButton.setOnClickListener{
+            setContentView(R.layout.activity_activities)
         }
 
     }
+
+
+
+    private fun searchActivityByType(query: String) {
+
+        //Buscar eso en la API
+
+        CoroutineScope(Dispatchers.IO).launch {
+
+            val call = getRetrofit()
+                .create(APIServiceActivity::class.java)
+                .getActivityByType("?type=$query")
+                if(call.isSuccessful) {
+                    //show recycler view
+                } else {
+                    //show error
+                }
+
+            //Cargar la respuesta en el Recycler View
+
+            val activityResponse : ActivityResponse? = call.body()
+
+
+            }
+
+        }
+
+    }
+
     private fun getRetrofit(): Retrofit {
 
         return Retrofit.Builder()
-            .baseUrl("http://www.boredapi.com/api/activity/")
+            .baseUrl("http://www.boredapi.com/api/activity")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
     }
-}
