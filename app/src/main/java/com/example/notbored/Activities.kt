0ab299ca418efa9df4c4d9512f71212396bf3d2c
Daniel-Cetina <daniel.cetina@mercadolibre.com.co>
2022-01-03 16:05:27 -0500

    New version using viewbinding

--- app/src/main/java/com/example/notbored/Activities.kt
+++ app/src/main/java/com/example/notbored/Activities.kt
@@ -2,11 +2,16 @@ package com.example.notbored
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.notbored.databinding.ActivityMainBinding
+import retrofit2.Retrofit
 
 class Activities : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_activities)
+        setContentView(binding.root)
 
 
 
--- app/src/main/java/com/example/notbored/MainActivity.kt
+++ app/src/main/java/com/example/notbored/MainActivity.kt
@@ -4,24 +4,25 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.example.notbored.databinding.ActivityMainBinding
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val buttonTermsAndConditions = findViewById<Button>(R.id.termsAndConditions)
-        buttonTermsAndConditions.setOnClickListener {
-            val termsAndConditionsIntent = Intent(this, TermsAndConditions::class.java)
-            startActivity(termsAndConditionsIntent)
+        binding.termsAndConditions.setOnClickListener{
+            setContentView(R.layout.activity_terms_and_conditions)
         }
 
-        val buttonStart = findViewById<Button>(R.id.startButton)
-        buttonStart.setOnClickListener {
-            val startIntent = Intent(this, Activities::class.java)
-            startActivity(startIntent)
+        binding.startButton.setOnClickListener{
+            setContentView(R.layout.activity_activities)
         }
 
     }
